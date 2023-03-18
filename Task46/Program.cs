815583897e3f6aadb3b5d59e8c3ebb8428cc1f46
Aoi <naztya2001@bk.ru>
2023-03-18 11:06:11 +0300

    task48

---
+++ Task46/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows,columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns
+        {
+            matrix[i,j] = rnd.Next(min,max+1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("(^3^)/");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 4} ");
+        }
+        Console.WriteLine("(^3^)/");
+    }
+}
+
+int[,] arr = CreateMatrix(3,4,1,20);
+PrintMatrix(arr);
+
+
+
---
+++ Task48/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows,columns];
+    for (int i = 0; i < matrix.GetLength(0); i++) //rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns
+        {
+            matrix[i,j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("(^3^)/");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j], 4} ");
+        }
+        Console.WriteLine("(^3^)/");
+    }
+}
+
+Console.WriteLine("Input rows and columns");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = CreateMatrix(m,n);
+PrintMatrix(arr);
